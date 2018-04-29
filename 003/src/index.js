65163c2edff956d5267dcb2e70ec1a054fb30bfd
784203108@qq.com <784203108@qq.com>
2018-04-29 17:32:10 +0800

    task3

---
+++ 003/src/index.js
@@ -0,0 +1,9 @@
+
+// 引入全局css
+// import style from "./main.css";
+// san, san-router与san的组件
+import san from "san";
+import {router, Link} from "san-router";
+import App from "./Components/App.san";
+router.add({rule: '/', Component: App, target: 'body'});
+router.start();
